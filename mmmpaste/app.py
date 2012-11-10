85bee8f74d75ccf12fa84c20bd7fa1eb440bf0a0
Ryan Cavicchioni <ryan@confabulator.net>
2012-11-10 15:27:11 -0600

    Spacing fix.

--- mmmpaste/app.py
+++ mmmpaste/app.py
@@ -32,6 +32,7 @@ def page_not_found(e):
     error = "The system is down. The system is down."
     return render_template("error.html", error = error), 500
 
+
 @app.route("/")
 def root():
     return redirect(url_for("new_paste"))
