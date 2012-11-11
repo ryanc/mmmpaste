d8dbd8fadc51342d76cede3c8d3719a22903d853
Ryan Cavicchioni <ryan@confabulator.net>
2012-11-11 13:55:27 -0600

    Import the default settings.

--- mmmpaste/app.py
+++ mmmpaste/app.py
@@ -7,6 +7,7 @@ from mmmpaste import forms
 from functools import update_wrapper
 
 app = Flask(__name__, instance_relative_config = True)
+app.config.from_object("mmmpaste.default_settings")
 app.config.from_object("local_settings")
 
 
--- mmmpaste/default_settings.py
+++ mmmpaste/default_settings.py
@@ -1,1 +1,2 @@
 DATABASE = 'sqlite:///db/pastebin.db'
+CACHE_S_MAXAGE = 86400
