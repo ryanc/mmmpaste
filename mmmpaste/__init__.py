9e2cd95617121ecbf71071fb8bfd6b5d24ec5966
Ryan Cavicchioni <ryan@confabulator.net>
2012-11-15 21:09:00 -0600

    Use instance relative configuration.

--- mmmpaste/__init__.py
+++ mmmpaste/__init__.py
@@ -2,6 +2,6 @@ from flask import Flask
 app = Flask(__name__, instance_relative_config = True)
 
 app.config.from_object("mmmpaste.default_settings")
-app.config.from_object("local_settings")
+app.config.from_pyfile("application.cfg", silent = True)
 
 import mmmpaste.views
