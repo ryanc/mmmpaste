c70f8aedf6a58ceb0fcd0e6f9c3ef1d5e698b8ca
Ryan Cavicchioni <ryan@confabulator.net>
2012-11-15 18:27:33 -0600

    Added a decorator to calculate the runtime of an action.

--- mmmpaste/filters.py
+++ mmmpaste/filters.py
@@ -1,6 +1,19 @@
 from flask import make_response
-from functools import update_wrapper
+from functools import update_wrapper, wraps
+from time import time as now
 from mmmpaste import app
+
+
+def runtime(f):
+    @wraps(f)
+    def wrapper(*args, **kwargs):
+        start = now()
+        response = make_response(f(*args, **kwargs))
+        end = now()
+        response.headers["X-Runtime"] = "%ss" % str(end - start)
+        return response
+    return wrapper
+
 
 def no_cache(f):
     def new_func(*args, **kwargs):
