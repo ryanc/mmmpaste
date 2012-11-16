216171e24485e233b9367e7aec6746428ff40e10
Ryan Cavicchioni <ryan@confabulator.net>
2012-11-15 20:26:10 -0600

    Renamed variables.

--- mmmpaste/filters.py
+++ mmmpaste/filters.py
@@ -5,39 +5,39 @@ from flask import make_response
 
 from mmmpaste import app
 
-def runtime(f):
+def runtime(fn):
     """
     Add a header that shows the runtime of the route.
     """
-    @wraps(f)
+    @wraps(fn)
     def wrapper(*args, **kwargs):
         start = now()
-        response = make_response(f(*args, **kwargs))
+        response = make_response(fn(*args, **kwargs))
         end = now()
         response.headers["X-Runtime"] = "{0}s".format(end - start)
         return response
     return wrapper
 
 
-def no_cache(f):
+def no_cache(fn):
     """
     Add "Cache-Control: no-cache" header.
     """
-    @wraps(f)
+    @wraps(fn)
     def wrapper(*args, **kwargs):
-        response = make_response(f(*args, **kwargs))
+        response = make_response(fn(*args, **kwargs))
         response.cache_control.no_cache = True
         return response
     return wrapper
 
 
-def cache(f):
+def cache(fn):
     """
     Add "Cache-Control: s-maxage" header.
     """
-    @wraps(f)
+    @wraps(fn)
     def wrapper(*args, **kwargs):
-        response = make_response(f(*args, **kwargs))
+        response = make_response(fn(*args, **kwargs))
         response.cache_control.s_maxage = app.config.get('CACHE_S_MAXAGE')
         return response
     return wrapper
