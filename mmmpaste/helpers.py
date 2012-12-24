7fc3867e7b8a01854116b43d9961e1063c051006
Ryan Cavicchioni <ryan@confabulator.net>
2012-12-23 22:23:55 -0600

    Select the first IP address from the X-Forwarded-For list.

--- mmmpaste/helpers.py
+++ mmmpaste/helpers.py
@@ -4,4 +4,4 @@ def get_ip():
     if not request.headers.get("X-Forwarded-For"):
         return request.remote_addr
 
-    return request.headers.get("X-Forwarded-For")
+    return request.headers.get("X-Forwarded-For")[0]
