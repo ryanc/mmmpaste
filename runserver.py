8ffdd3127d6226815b508bd10ccd84eec22e6d1c
Ryan Cavicchioni <ryan@confabulator.net>
2012-12-24 13:45:23 -0600

    Allow debug mode to be configured via an environment variable.

--- runserver.py
+++ runserver.py
@@ -4,5 +4,6 @@ from mmmpaste import app
 
 # Get the port from the enviroment or fall back to the default.
 PORT = int(os.environ.get('PORT', 5000))
+DEBUG = bool(os.environ.get('PORT', True))
 
-app.run(debug = True, port = PORT)
+app.run(debug = DEBUG, port = PORT)
